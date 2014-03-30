d415b4e93b3b98b136ec5123bc516dbe0ae4aedc
Ryan Weber <Weber@Ryans-MacBook-Pro-2.local>
2014-03-30 17:16:19 -0400

    test

--- test/public/core.js
+++ test/public/core.js
@@ -244,7 +244,28 @@ $(document).ready(function(){
  //    	}
 	// });
 
-	$('.jstree-anchor').contextMenu();
+	$(function(){
+	    $.contextMenu({
+	        selector: '.jstree-anchor', 
+	        callback: function(key, options) {
+	            var m = "clicked: " + key;
+	            window.console && console.log(m) || alert(m); 
+	        },
+	        items: {
+	            "edit": {name: "Edit", icon: "edit"},
+	            "cut": {name: "Cut", icon: "cut"},
+	            "copy": {name: "Copy", icon: "copy"},
+	            "paste": {name: "Paste", icon: "paste"},
+	            "delete": {name: "Delete", icon: "delete"},
+	            "sep1": "---------",
+	            "quit": {name: "Quit", icon: "quit"}
+	        }
+	    });
+	    
+	    $('.context-menu-one').on('click', function(e){
+	        console.log('clicked', this);
+	    })
+	});
 
 });
 
